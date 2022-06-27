dd5cfcb68429efc75ab13801b95fd7b262a0b069
pochiman <pochiman@yahoo.com>
2022-06-27 14:59:36 -0400

    first commit

--- 03_Hard/025_Find_Loop.py
+++ 03_Hard/025_Find_Loop.py
@@ -60,7 +60,7 @@ O(n) time | O(1) space - where n is the number of nodes in the Linked List
 
 
 ##### Solution 1 #####
-# This is an input class. Do not edit.
+# This is an input class.  Do not edit.
 class LinkedList:
   def __init__(self, value):
     self.value = value
