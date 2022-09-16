b5c2e56eaf58361bb6d4fc567f27be97ecc77daa
pochiman <pochiman@yahoo.com>
2022-09-16 09:47:45 -0400

    first commit

--- 01_Easy/001_Two_Number_Sum.py
+++ 01_Easy/001_Two_Number_Sum.py
@@ -50,13 +50,13 @@ O(n) time | O(n) space - where n is the length of the input array
 ##### Solution 1 #####
 # O(n^2) time | O(1) space
 def twoNumberSum(array, targetSum):
-  for i in range(len(array) - 1):
-    firstNum = array[i]
-    for j in range(i + 1, len(array)):
-      secondNum = array[j]
-      if firstNum + secondNum == targetSum:
-        return [firstNum, secondNum]
-  return []
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
 
 
 
@@ -65,14 +65,14 @@ def twoNumberSum(array, targetSum):
 ##### Solution 2 #####
 # O(n) time | O(n) space
 def twoNumberSum(array, targetSum):
-  nums = {}
-  for num in array:
-    potentialMatch = targetSum - num
-    if potentialMatch in nums:
-      return [potentialMatch, num]
-    else:
-      nums[num] = True
-  return []
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []
 
 
 
@@ -81,16 +81,15 @@ def twoNumberSum(array, targetSum):
 ##### Solution 3 #####
 # O(nlog(n)) | O(1) space
 def twoNumberSum(array, targetSum):
-  array.sort()
-  left = 0
-  right = len(array) - 1
-  while left < right:
-    currentSum = array[left] + array[right]
-    if currentSum == targetSum:
-      return [array[left], array[right]]
-    elif currentSum < targetSum:
-      left += 1
-    elif currentSum > targetSum:
-      right -= 1
-  return []
-  
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
